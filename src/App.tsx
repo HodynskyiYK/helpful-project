5bec0c9d8974cb37716d6d737cde7f4c4773b162
HodynskyiYK <hodynskyiyk@gmail.com>
2023-08-30 23:23:47 +0300

    fixed temporary test in App.test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {Layout, Main, Other, NoMatch} from './pages'
 
 
 const App = () => {
 
     return (
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<Main />} />
-                <Route path="other" element={<Other />} />
-                <Route path="*" element={<NoMatch />} />
-            </Route>
-        </Routes>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Main />} />
+                    <Route path="other" element={<Other />} />
+                    <Route path="*" element={<NoMatch />} />
+                </Route>
+            </Routes>
+        </Router>
     )
 }
 
